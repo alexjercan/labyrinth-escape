05903b560a5f7ade0868ba1dff5af9c5ddc3f01f
Alex Jercan <jercan_alex27@yahoo.com>
2023-02-12 14:51:38 +0200

    format

--- index.js
+++ index.js
@@ -5,27 +5,34 @@ const width = 23;
 const height = 23;
 const cellSize = 20;
 
-const canvas = document.createElement('canvas');
-canvas.id     = "CursorLayer";
-canvas.width  = width * cellSize;
+const canvas = document.createElement("canvas");
+canvas.id = "CursorLayer";
+canvas.width = width * cellSize;
 canvas.height = height * cellSize;
-canvas.style.zIndex   = 8;
+canvas.style.zIndex = 8;
 canvas.style.position = "absolute";
-canvas.style.border   = "1px solid";
+canvas.style.border = "1px solid";
 
 const gameDiv = document.getElementById("game");
 gameDiv.appendChild(canvas);
 
-const context = canvas.getContext('2d');
+const context = canvas.getContext("2d");
 const cells = primMaze(width, height);
 for (let row = 0; row < height; row++) {
-    for (let col = 0; col < width; col++) {
-        const contains = cells.filter((point) => point.row == row && point.col == col).length != 0;
-        const color = contains ? "#808080" : "#000000";
-        const cellRenderer = new RectRenderer(color, cellSize / 2, cellSize / 2, cellSize, cellSize);
+  for (let col = 0; col < width; col++) {
+    const contains =
+      cells.filter((point) => point.row == row && point.col == col).length != 0;
+    const color = contains ? "#808080" : "#000000";
+    const cellRenderer = new RectRenderer(
+      color,
+      cellSize / 2,
+      cellSize / 2,
+      cellSize,
+      cellSize
+    );
 
-        context.translate(col * cellSize, row * cellSize);
-        cellRenderer.draw(context);
-        context.translate(-1 * col * cellSize, -1 * row * cellSize);
-    }
+    context.translate(col * cellSize, row * cellSize);
+    cellRenderer.draw(context);
+    context.translate(-1 * col * cellSize, -1 * row * cellSize);
+  }
 }
--- src/engine/renderer.js
+++ src/engine/renderer.js
@@ -1,23 +1,23 @@
 export class RectRenderer {
-    constructor(fillStyle, x, y, width, height) {
-        this.fillStyle = fillStyle;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
+  constructor(fillStyle, x, y, width, height) {
+    this.fillStyle = fillStyle;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
 
-    draw(context) {
-        context.translate(-1 * this.width / 2, -1 * this.height / 2);
+  draw(context) {
+    context.translate((-1 * this.width) / 2, (-1 * this.height) / 2);
 
-        context.lineWidth = 1;
-        context.lineCap = "butt";
-        context.strokeStyle = "black";
-        context.strokeRect(this.x, this.y, this.width, this.height);
+    context.lineWidth = 1;
+    context.lineCap = "butt";
+    context.strokeStyle = "black";
+    context.strokeRect(this.x, this.y, this.width, this.height);
 
-        context.fillStyle = this.fillStyle;
-        context.fillRect(this.x, this.y, this.width, this.height);
+    context.fillStyle = this.fillStyle;
+    context.fillRect(this.x, this.y, this.width, this.height);
 
-        context.translate(this.width / 2, this.height / 2);
-    }
+    context.translate(this.width / 2, this.height / 2);
+  }
 }
