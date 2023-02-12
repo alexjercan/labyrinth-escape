ac55e7011873392c5be48e7392d0441ee0a513c0
Alex Jercan <jercan_alex27@yahoo.com>
2023-02-12 15:49:31 +0200

    wip

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ import { primMaze, isContained } from "./src/primMaze.js";
 const width = 47;
 const height = 47;
 const cellSize = 20;
+const playerStepMilliseconds = 100;
 
 // Scene Stuff
 let context = null;
 let cells = null;
 let player = null;
 let humanInput = null;
+let prevTimestamp = null;
 
-function init() {
+function init({ timeStamp }) {
   const canvas = document.createElement("canvas");
   context = canvas.getContext("2d");
 
@@ -31,14 +33,20 @@ function init() {
 
   player = {
     position: { row: Math.floor(height / 2), col: Math.floor(width / 2) },
+    stepMilliseconds: 0,
   };
 
   humanInput = new HumanInput(document);
 
+  prevTimestamp = timeStamp;
+
   window.requestAnimationFrame(loop);
 }
 
-function loop(dt) {
+function loop(timestamp) {
+  const deltaTime = timestamp - prevTimestamp;
+  prevTimestamp = timestamp;
+
   for (let row = 0; row < height; row++) {
     for (let col = 0; col < width; col++) {
       const contains = isContained({ row, col }, cells);
@@ -71,9 +79,11 @@ function loop(dt) {
   context.translate(-1 * col * cellSize, -1 * row * cellSize);
 
   // Input
+  player.stepMilliseconds -= deltaTime;
   const [dx, dy] = humanInput.waitInput();
   const newPosition = { row: row - dx, col: col + dy };
-  if (isContained(newPosition, cells)) {
+  if (isContained(newPosition, cells) && player.stepMilliseconds <= 0) {
+    player.stepMilliseconds = playerStepMilliseconds;
     player.position = newPosition;
   }
 
